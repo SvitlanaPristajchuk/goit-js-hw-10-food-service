bd031d66aab14a8c7cd9fb14a9dab5f7b5294adf
Svetlana Pristaychuk <73787800+SvitlanaPristajchuk@users.noreply.github.com>
2021-08-18 22:01:51 +0300

    last v

--- src/index.js
+++ src/index.js
@@ -10,9 +10,28 @@ const markup = card.map(templatesCard).join('')
 console.log(markup)
 
 const refs  = {
-    markCard: document.querySelector('.js-menu')
+    markCard: document.querySelector('.js-menu'),
+    body: document.querySelector('body'),
+    input: document.querySelector('#theme-switch-toggle')
 }
 
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
 
+refs.markCard.insertAdjacentHTML('beforeend', markup);
 
-refs.markCard.insertAdjacentHTML('beforeend', markup)
+//refs.input.addEventListener('change', () => {})
+refs.input.addEventListener('change', onInputChange);
+
+ function onInputChange() {
+  if (refs.input.checked) {
+    refs.body.classList.remove(Theme.LIGHT);
+    refs.body.classList.add(Theme.DARK);
+    localStorage.setItem('theme', Theme.DARK);
+  } else {
+    refs.body.classList.replace(Theme.DARK, Theme.LIGHT);
+    localStorage.setItem('theme', Theme.LIGHT);
+  }
+};
